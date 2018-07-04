873adae53484d2cd1574bd049214c31d9f72d118
13abel <richard.wei@mail.utoronto.ca>
2018-07-04 09:02:44 -0400

    added a dialog fragment and code for displaying it when the title is clicked

---
+++ app/src/main/java/com/relic/presentation/displaysub/DisplaySubInfoDialog.java
@@ -0,0 +1,35 @@
+package com.relic.presentation.displaysub;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+public class DisplaySubInfoDialog extends DialogFragment{
+
+  @NonNull
+  @Override
+  public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+    //
+    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+    builder.setMessage("TEST")
+        .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialogInterface, int i) {
+            // TODO set yes
+
+          }
+        }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialogInterface, int i) {
+            // TODO set no
+          }
+        });
+    return super.onCreateDialog(savedInstanceState);
+  }
+
+
+}
--- app/src/main/java/com/relic/presentation/displaysub/DisplaySubView.java
+++ app/src/main/java/com/relic/presentation/displaysub/DisplaySubView.java
@@ -1,16 +1,20 @@
 package com.relic.presentation.displaysub;
 
+import android.app.Dialog;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -175,12 +179,14 @@ public class DisplaySubView extends Fragment {
       TextView subTitle = getActivity().findViewById(R.id.my_toolbar_subtitle);
 
       title.setText(subName);
-      title.setText("Popular");
+      subTitle.setText("Popular");
 
       title.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
           Toast.makeText(getContext(), "Title Clicked", Toast.LENGTH_SHORT).show();
+          DisplaySubInfoDialog dialog = new DisplaySubInfoDialog();
+          dialog.show(getFragmentManager(), TAG);
         }
       });
     }
