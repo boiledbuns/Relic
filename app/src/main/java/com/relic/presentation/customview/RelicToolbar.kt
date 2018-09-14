b0902b4b2d436d82a17a70acf82301ea60506ed1
13abel <richard.wei@mail.utoronto.ca>
2018-09-13 23:56:47 -0400

    added basis for custom view class for styling in toolbar in the subview

---
+++ app/src/main/java/com/relic/presentation/customview/RelicToolbar.kt
@@ -0,0 +1,13 @@
+package com.relic.presentation.customview
+
+import android.content.Context
+import android.support.v7.widget.Toolbar
+import android.util.AttributeSet
+
+class RelicToolbar @JvmOverloads constructor(
+    context: Context,
+    attrs : AttributeSet? = null,
+    defStyleAttr : Int = 0
+): Toolbar (context, attrs, defStyleAttr){
+    
+}
